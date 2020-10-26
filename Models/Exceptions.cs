0028294f985ddd63507ef37cc7d5639edc43f068
ChungLingTsao <39504754+ChungLingTsao@users.noreply.github.com>
2020-10-26 23:51:28 +1300

    Add files via upload

---
+++ Models/Exceptions.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * Exceptions.cs
+ * Author: Chung-Ling Tsao (92019797)
+ * Description: The exception class that will throw an exception for a failed withdrawl.
+*/
+using System;
+
+namespace Assessment2
+{
+    [Serializable]
+    public class FailedWithdrawlException : Exception
+    {
+        public FailedWithdrawlException()
+        {
+        }
+
+        public FailedWithdrawlException(double debit)
+            : base(String.Format("Invalid Debit Withdrawl Amount: {0}", debit))
+        {
+        }
+    }
+
+    [Serializable]
+    public class InvalidCustomerException : Exception
+    {
+        public InvalidCustomerException()
+            : base(String.Format("Invalid Customer"))
+        {
+        }
+    }
+}
